a283e572de42e615cc8da298a8320ff9336efda7
bhagyashree-watpade <bhagyashreewatpade@gmail.com>
2025-03-27 14:45:52 +0530

    Updated common functionality in the setup

---
+++ database/connection.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+DATABASE_URL = "postgresql://postgres:a@localhost/Employee_Appraisal"
+
+# Create database engine
+engine = create_engine(DATABASE_URL)
+
+# Session factory
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base model class
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()         # FastAPI calls the function and gets the session from sessionLocal
+    try:
+        yield db                # Then it temporarily pause and use this session (inject it into route)
+    finally:
+        db.close()              # After the use is done, it comes back and close the session
+
--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("hi")
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Enable CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Allow requests from React app
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+#existing routes
