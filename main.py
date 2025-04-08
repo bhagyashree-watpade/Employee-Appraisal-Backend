eec21e1c7a264dba341f6cbd26609254cfb1a230
bhagyashree-watpade <160125999+bhagyashree-watpade@users.noreply.github.com>
2025-04-08 17:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ app.add_middleware(
     # allow_origins=["http://localhost:3000"], 
     # allow_origins=["*"],# Allow requests from React app
      # allow_origins=["https://employee-appraisal-frontend-kz69.vercel.app"],  #akanksha
-    allow_origins=["https://employee-appraisal-frontend-finalllllllllll.vercel.app"]   #bhagyashree
+    allow_origins=["https://employee-appraisal-frontend-finalllllllllll.vercel.app"],   #bhagyashree
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
