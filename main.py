32fab44f6bb8cfc43746927c920b873598935193
bhagyashree-watpade <160125999+bhagyashree-watpade@users.noreply.github.com>
2025-04-08 16:54:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ app = FastAPI()
 # Enable CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow requests from React app
+    # allow_origins=["http://localhost:3000"], 
+    allow_origins=["*"],# Allow requests from React app
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
