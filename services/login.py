0272a137862152107b8dab71d2ebfe90c6b9fdc7
bhagyashree-watpade <160125999+bhagyashree-watpade@users.noreply.github.com>
2025-04-22 12:41:25 +0530

    Added a backend to login based on the role id

--- services/login.py
+++ services/login.py
@@ -1,10 +1,11 @@
 from sqlalchemy.orm import Session
-from dao.employee import get_employee_by_id
+from dao.employee import get_employee_by_id, get_employee_by_role_id
 
 def authenticate_employee(db: Session, employee_id: int, password: str):
     """Authenticate employee by checking if the provided password matches the stored one."""
-    employee = get_employee_by_id(db, employee_id)
-    
+    # employee = get_employee_by_id(db, employee_id)     # for login based on employee ID
+    employee = get_employee_by_role_id(db, employee_id) # for login based on role ID
+
     if not employee:
         return None  # Employee not found
     
