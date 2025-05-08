9f29108174c9e7a025a78866a00121b4f4eb696b
bhagyashree-watpade <160125999+bhagyashree-watpade@users.noreply.github.com>
2025-05-08 12:28:35 +0530

    Modified the question routes to optimize the time

--- dao/questions.py
+++ dao/questions.py
@@ -1,4 +1,4 @@
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import Session, selectinload
 from models.questions import Question, Option
 from schema.questions import QuestionSchema
 from typing import List
@@ -6,8 +6,14 @@ from typing import List
 
 def get_all_questions(db: Session) -> List[Question]:
     #Fetch all questions from the database
-    return db.query(Question).all()
+    # print("All questions fetched", db.query(Question).all())
+    # return db.query(Question).all()
+    return db.query(Question.question_id, Question.question_text).all()
 
+
+def get_all_questions_with_option(db: Session) -> List[Question]:
+    #Fetch all questions with their options from the database
+   return db.query(Question).options(selectinload(Question.options)).all()
 
 def add_new_question(question_data: QuestionSchema, db: Session) -> Question:
     #Add a new question to the database
